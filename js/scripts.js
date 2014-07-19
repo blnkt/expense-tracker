c9c416ceb4d06b29a1e813f34bff93ba19fe7c5d
Ellen Krause Lehman & Blake Mason <student@epicodus.com>
2014-07-18 17:00:44 -0700

    working

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,62 @@
+var people = [];
 
-  var people = [];
-  var allPossessions =[];
-  var possession = {name:"", possessionScore:0};
-  var Person = {name:"", personPosessions:allPossessions, yourScore: function(){
+var Possession = {
+  name: "",
+  possessionScore: 0
+
+};
+var Person = {
+  name: "",
+  yourScore: function(){
     var totalScore = 0;
-    Person.personPossessions.forEach(function(eachPossession) {
+    this.possessions.forEach(function(eachPossession) {
       totalScore += eachPossession.possessionScore;
     });
     return totalScore;
-  };
+  }
 };
 
+
+
 $(document).ready(function() {
-  $("form#").click(function() {
+  var currentPerson;
+  $("#enter-your-name").click(function(event) {  //click for making people
     var newPerson = Object.create(Person);
-    var newPossessions
+    newPerson.possessions = [];
+    newPerson.name = $("input.new-name").val();
+    $("ul.people").append("<li><span class='clickable'>" + newPerson.name + "</span></li>");
+    people.push(newPerson);
+    $("input.new-name").val("");
+
+    $(".clickable").last().click(function() {  //click for showing person's possessions
+      $(".person-name").text(newPerson.name);
+      currentPerson = newPerson;
+      // console.log(currentPerson);
+      // console.log(people);
+      $("ul.possessions").empty();
+      currentPerson.possessions.forEach(function(eachPossession){
+        $("ul.possessions").append("<li>" + eachPossession.name + " = " + eachPossession.possessionScore + "</li>");
+      });
+    $("#second-column").show();
+    });
+  });
+
+  $("#add-possession").last().click(function() {  //click for add new possessions to person's list
+    var newPossession = Object.create(Possession);
+    newPossession.name = $("input.new-possession").val();
+    newPossession.possessionScore = parseInt($("select.possession-score").val());
+    currentPerson.possessions.push(newPossession);
+    $("input.new-possession").val("");
+    $("select.possession-score").val($("#default-option").val());
+console.log(currentPerson);
+    $("ul.possessions").empty();
+    currentPerson.possessions.forEach(function(eachPossession){
+      $("ul.possessions").append("<li>" + eachPossession.name + " = " + eachPossession.possessionScore + "</li>");
+    });
+    $("#third-column").show();
+  });
+
+  $(".total-score").click(function() {
+    $(".your-score").text(currentPerson.yourScore());
+  });
 });
